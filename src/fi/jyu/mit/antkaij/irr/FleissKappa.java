8b5b48bb5d9d28640bb75bb64cc25ec64527701c
Antti-Juhani Kaijanaho <antti-juhani@kaijanaho.fi>
2014-10-21 15:27:10 +0300

    Fleiss kappa variance, CIs and significance tests
    Signed-off-by: Antti-Juhani Kaijanaho <antti-juhani@kaijanaho.fi>

--- src/fi/jyu/mit/antkaij/irr/FleissKappa.java
+++ src/fi/jyu/mit/antkaij/irr/FleissKappa.java
@@ -34,25 +34,47 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 
-/* References:
+/* Reference for the statistic itself:
 
    Joseph L. Fleiss (1971).  Measuring Nominal Scale Agreement Among
    Many Raters. Psychological Bulletin 76 (5), 378-382.
 
-   (Note that the variance formula given by Fleiss 1971 is incorrect.)
+   Regarding variance, that source is incorrect, as pointed out by
 
    Joseph L. Fleiss & John C. M. Nee & J. Richard Landis (1979).
    Large Sample Variance of Kappa in the Case of Different Sets of
    Raters.  Psychological Bulletin 86 (5), 974-977.
 
+   Their formula, in turn, assumes random allocation and thus is only
+   applicable to testing the null hypothesis 𝜅 = 0.  What I need here
+   is, however, a variance formula that makes no such assumption, for
+   constructing confidence intervals and for performing hypthesis
+   testing with a variety of null hypotheses.  The following paper
+   presents one such variance formula, and I use it here.
+
+   Kilem Li Gwet (2008).  Computing inter-rater reliability and its
+   variance in the presence of high agreement.  British Journal of
+   Mathematical and Statistical Psychology 61 (1), 29-48.
+   doi:10.1348/000711006X126600
+
+
  */
 
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+import static java.lang.Math.sqrt;
 import static java.util.Arrays.asList;
+
+import static fi.jyu.mit.antkaij.irr.Util.cdf;
+import static fi.jyu.mit.antkaij.irr.Util.cdfinv;
 
 public class FleissKappa implements ReliabilityStatistic {
     private final String variableName;
     private final double value;
-
+    private final double variance;
+    private final double se;
+    private final int n;
+    
     public FleissKappa(DataMatrix dm) {
         variableName = dm.variableName;
 
@@ -72,6 +94,7 @@ public class FleissKappa implements ReliabilityStatistic {
             }
             ++N;
         }
+        this.n = N;
 
         double Pbar = 0;
         for (int i = 0; i < N; i++) {
@@ -92,6 +115,35 @@ public class FleissKappa implements ReliabilityStatistic {
         }
 
         value = (Pbar - Pe) / (1 - Pe);
+
+        // Variance according to Gwet, formula 33.
+
+        // Gwet's i is our i
+        // Gwet's k is our j
+        // Gwet's n is our N
+        // Gwet's q is our k
+        // Gwet's r is our n
+        // Gwet's pe|pi is our Pe
+        // Gwet's gamma(hat)pi is our value
+        // I will ignore Gwet's f (treat it as f = 0)
+
+        double vsum = 0;
+        for (int i = 0; i < N; i++) {
+            double pai = 0;
+            double pei = 0;
+            for (int j = 0; j < k; j++) {
+                pai += (nij[i][j]*(nij[i][j]-1))/(n*(n-1));
+                pei += nij[i][j]/n;
+            }
+            pei *= value;
+
+            double gi = (pai - Pe) / (1 - Pe);
+
+            double gistar = gi - 2 * (1 - value) * (pei - Pe) / (1 - Pe);
+            vsum += (gistar - value) * (gistar - value);
+        }
+        variance =  vsum / (N*(N-1));
+        se = sqrt(variance);
     }
 
     public String name()   { return "Fleiss' Kappa"; }
@@ -102,14 +154,25 @@ public class FleissKappa implements ReliabilityStatistic {
     }
     public double pointEstimate() { return value; }
     public ConfidenceInterval confidenceInterval(double p) {
+        double z = cdfinv(1-(1-p)/2);
         return new ConfidenceInterval(p,
-                                      Double.NaN,
-                                      Double.NaN,
-                                      "not implemented");
+                                      max(-1, value - z * se),
+                                      min(+1, value + z * se));
     }
     public PValue pValue(double minValue) {
-        return new PValue(Double.NaN, "not implemented");
+        double z = (value - minValue) / se;
+        return new PValue(1 - cdf(z), "z", z, "upper tail");
     }
     public void printAdditionalInfo(Writer w) throws IOException {
+        w.write(String.format("variance = %.5f\n", variance));
+        w.write(String.format("n = %d\n", n));
+        if (n < 30) {
+            w.write("\nWARNING: The sample is small.\n");
+            w.write("The assumptions underlying the variance are probably " +
+                    "invalid.\n");
+            w.write("This means the CONFIDENCE INTERVALS and SIGNIFICANCE " +
+                    "TESTS are\n");
+            w.write("probably INVALID as well.\n");
+        }
     }
 }
